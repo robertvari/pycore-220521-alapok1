da7ef6c4b116d63693e4e8d795b82b73d1b75034
Robert Vári <mail.robertvari@gmail.com>
2022-05-21 17:59:37 +0200

    update

---
+++ 09_if_and.py
@@ -0,0 +1,16 @@
+username = "robert"
+password = "testpas123"
+
+
+input_username = input("Username?")
+input_password = input("Password?")
+
+#            True                         True
+if username == input_username and password == input_password:
+    print(f"You are logged in: {username}")
+else:
+    if username != input_username:
+        print("Username was wrong. Try again!")
+
+    if password != input_password:
+        print("Password was wrong. Try again!")
