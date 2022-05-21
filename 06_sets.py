068cfc596415d9670bc9e23d7b259f47606f8a9b
Robert Vári <mail.robertvari@gmail.com>
2022-05-21 17:10:34 +0200

    update

--- 06_sets.py
+++ 06_sets.py
@@ -1,16 +1,31 @@
 my_set = {1, 2, 3}
-print(my_set)
+# print(my_set)
 
 # add items to set
 my_set.add(4)
-print(my_set)
+# print(my_set)
 
 my_set.update([3, 4, 5, 6, 7])
-print(my_set)
+# print(my_set)
 
 # remove items from set
 my_set.discard(20)
-print(my_set)
+# print(my_set)
 
-my_set.remove(20)
-print(my_set)
+my_set.remove(5)
+# print(my_set)
+
+
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7, 8}
+# Union
+print(A | B)
+
+# intersection
+print(A & B)
+
+# difference
+print(A - B)
+
+# symmetrical difference
+print(A ^ B)
