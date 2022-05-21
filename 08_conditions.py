ef54dd1e934074c8db79cced088c548a29896370
Robert Vári <mail.robertvari@gmail.com>
2022-05-21 17:50:13 +0200

    update

---
+++ 08_conditions.py
@@ -0,0 +1,14 @@
+number = 10
+
+# number == 10
+# number < 10
+# number > 10
+# number <= 10
+# number >= 10
+# number != 10
+
+if number != 10:
+    print("Number is < 10")
+    print("This is also true...")
+else:
+    print("number is >= 10")
