b8eab9b0f1267959b00dad417961a93fd0821633
Robert Vári <mail.robertvari@gmail.com>
2022-05-21 17:03:35 +0200

    update

---
+++ 06_sets.py
@@ -0,0 +1,16 @@
+my_set = {1, 2, 3}
+print(my_set)
+
+# add items to set
+my_set.add(4)
+print(my_set)
+
+my_set.update([3, 4, 5, 6, 7])
+print(my_set)
+
+# remove items from set
+my_set.discard(20)
+print(my_set)
+
+my_set.remove(20)
+print(my_set)
