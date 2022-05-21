51f505ef0b18871a577c26029da3f427b88072ee
Robert Vári <mail.robertvari@gmail.com>
2022-05-21 15:55:43 +0200

    update

---
+++ 04_string_formating.py
@@ -0,0 +1,5 @@
+user_name = input("What is your name?")
+user_email = input("What is your email?")
+
+# formatted string: f"...{variable_name}"
+print(f"User name is: {user_name}. Email is: {user_email}")
