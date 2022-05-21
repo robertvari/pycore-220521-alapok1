57f0ec30e1fdd75f9f4a8945c5bb35cc8b20214b
Robert Vári <mail.robertvari@gmail.com>
2022-05-21 15:43:24 +0200

    update

---
+++ 03_variables.py
@@ -0,0 +1,14 @@
+my_name = "Robert Vari"
+my_age = 42
+my_address = "Budapest"
+my_email = "mail.pythonsuli@gmail.com"
+
+
+print(my_name)
+print(my_age)
+
+my_age = 18
+print(my_age)
+
+print(my_address)
+print(my_email)
