bb0a40b4db8d694a1d28c8d3aeb2d651258b9b4e
Robert Vári <mail.robertvari@gmail.com>
2022-05-21 15:07:47 +0200

    init project

---
+++ 01_datatypes.py
@@ -0,0 +1,17 @@
+# booleans
+True, False
+0, 1
+
+# numbers: integers, floats
+# int: integer
+12, 200, -200
+
+# floats
+3.014 -12.12345678
+
+# strings: str
+"Vári Róbert"
+"mail.pythonsuli@gmail.com"
+
+
+# ...
