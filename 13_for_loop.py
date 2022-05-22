0aa9ebb4f2c505d8fa4c8c15320b76166a793175
Robert Vári <mail.robertvari@gmail.com>
2022-05-22 14:36:41 +0200

    update

---
+++ 13_for_loop.py
@@ -0,0 +1,17 @@
+names = ["Robert", "Csaba", "Csilla", "Anna"]
+
+# for i in names:
+#     print(i)
+
+# break
+# for i in names:
+#     if i == "Csilla":
+#         print(i)
+#         break
+
+
+# continue
+for i in names:
+    if i == "Csaba":
+        continue
+    print(i)
