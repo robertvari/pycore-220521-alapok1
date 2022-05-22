cd97781d3e8cd11d9c66ac3e70ee7683dfcdd8f0
Robert Vári <mail.robertvari@gmail.com>
2022-05-22 16:48:44 +0200

    update

--- PhotoToExeld/PhotoToExcel.py
+++ PhotoToExeld/PhotoToExcel.py
@@ -1,4 +1,6 @@
 import os
+from PIL import Image, ExifTags
+
 
 photo_folder = r"C:\Work\_PythonSuli\pycore-220521\photos"
 
@@ -21,5 +23,24 @@ for i in folder_content:
     if extension.lower() in allowed_extensions:
         full_path = os.path.join(photo_folder, i)
         image_files.append(full_path)
+
+
+photo_data = {}
+
+for file_path in image_files:
+    img = Image.open(file_path)
+    image_size = img.size
+
+    # get metadata from image
+    exif_data = img._getexif()
+
+    photo_data[file_path] = {
+        "size": image_size,
+        "date": exif_data.get(0x9003) if exif_data else None,
+        "camera": exif_data.get(0x0110) if exif_data else None,
+        "iso": exif_data.get(0x8827) if exif_data else None
+    }
+
+
 
 pass
